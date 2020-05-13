31ca1db5bca2d8cab2b763eb4800b0243c3e35e9
Kamal Nayan <mailtokamalnayan@gmail.com>
2020-05-13 16:40:41 +0530

    Update URL of image

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -56,7 +56,7 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 function getDefaultImages(images: string[], theme: Theme): string[] {
   const defaultImage =
     theme === "light"
-      ? "https://remotejobs.store/RemoteJobsText.svg"
+      ? "https://remotejobs.store/logo_full.svg"
       : "https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg";
 
   if (!images || !images[0]) {
